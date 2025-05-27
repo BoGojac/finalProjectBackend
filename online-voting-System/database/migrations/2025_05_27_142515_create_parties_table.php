b220d76f94b00cae012b628f01c9a5eb9952bc7d
Adugna@BoGojac <adugnaolanigeleta@gmail.com>
2025-05-27 17:48:19 +0300

    recreation of migrations

---
+++ online-voting-System/database/migrations/2025_05_27_142515_create_parties_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('abbrevation');
+            $table->string('leader');
+            $table->date('foundation_year');
+            $table->string('partcipation_area');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parties');
+    }
+};
